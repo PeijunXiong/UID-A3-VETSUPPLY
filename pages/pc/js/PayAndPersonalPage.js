10738009e4c8cb84909f61de8c8dba779ef90b80
PeijunXiong <youlanda030www@gmail.com>
2024-05-27 21:33:31 +0800

    feat: pay page

--- pages/pc/js/PayAndPersonalPage.js
+++ pages/pc/js/PayAndPersonalPage.js
@@ -1,2 +1,15 @@
 import "./index.js"
 
+window.addEventListener("DOMContentLoaded", () => {
+    const doms = {
+        payBtn: document.querySelector(".p-main-btn"),
+        dialog: document.querySelector(".dialog"),
+        dialogOk: document.querySelector(".dialog-ok"),
+    }
+    doms.payBtn.addEventListener("click", () => {
+        doms.dialog.style.display = "block"
+    })
+    doms.dialogOk.addEventListener("click", () => {
+        doms.dialog.style.display = "none"
+    })
+})
