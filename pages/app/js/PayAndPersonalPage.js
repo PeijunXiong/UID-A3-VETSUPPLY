02302e1754f19aa1cf5e9a784548f66386dc57e7
PeijunXiong <youlanda030www@gmail.com>
2024-05-30 22:49:28 +0800

    feat: app

--- pages/app/js/PayAndPersonalPage.js
+++ pages/app/js/PayAndPersonalPage.js
@@ -1,3 +1,35 @@
-import "./index.js"
+import "./index.js";
 
-console.log("app");
+window.addEventListener("DOMContentLoaded", () => {
+  function bindEvent() {
+    const doms = {
+      downBtn: document.querySelector(".icon-down.down"),
+      upBtn: document.querySelector(".icon-down.up"),
+      contentDown: document.querySelector(".contentDown"),
+      contentUp: document.querySelector(".contentUp"),
+      payBtn: document.querySelector(".pay-btn"),
+      dialog: document.querySelector(".dialog"),
+      dialogOk: document.querySelector(".dialog-ok"),
+    };
+
+    doms.downBtn.addEventListener("click", () => {
+      doms.contentUp.style.display = "none";
+      doms.contentDown.style.display = "block";
+      doms.downBtn.style.display = "none";
+    });
+    doms.upBtn.addEventListener("click", () => {
+      doms.contentUp.style.display = "block";
+      doms.contentDown.style.display = "none";
+      doms.downBtn.style.display = "block";
+    });
+    doms.payBtn.addEventListener("click", () => {
+      doms.dialog.style.display = "block";
+    });
+    doms.dialogOk.addEventListener("click", () => {
+      doms.dialog.style.display = "none";
+      window.location.href = "./FinishedPage.html";
+    });
+  }
+
+  bindEvent();
+});
--- pages/app/js/ShoppingCartPage.js
+++ pages/app/js/ShoppingCartPage.js
@@ -1,3 +1,17 @@
-import "./index.js"
+window.addEventListener("DOMContentLoaded", () => {
+    function bindEvent() {
+      const doms = {
+        continueBtn: document.querySelector(".continue-btn"),
+        checkBtn: document.querySelector(".Checkout-btn"),
+      };
 
-console.log("app");
+      doms.continueBtn.addEventListener("click", () => {
+          window.location.href = "./HomePage.html"
+      })
+      doms.checkBtn.addEventListener("click", () => {
+          window.location.href = "./PayAndPersonalPage.html"
+      })
+    }
+  
+    bindEvent();
+  });
